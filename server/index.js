f8f44a48f1bd5515dc1f65596a8814b32af20548
Aryan Sahu <sahuaryan254@gmail.com>
2025-12-30 18:52:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-/* =========================
+/* =======================
    Middlewares
 ========================= */
 app.use(cors());
